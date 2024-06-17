4b3249f1acb24e9ae55e6bc53e47b1b46f585f37
Srinu Rachakonda <srinurachakonda126@gmail.com>
2024-06-17 15:54:16 -0400

    Updated comments for unit test cases

--- LaptopDiscountTest/UnitTest1.cs
+++ LaptopDiscountTest/UnitTest1.cs
@@ -5,8 +5,10 @@ namespace LaptopDiscountTest;
 public class Tests
 {
     [Test]
-        //Test case for part time employee. As PartTime is one of the employee type and
-        // the discount should be zero for part time employee
+    /*
+        Test case for part time employee. As PartTime is one of the employee type and
+        the discount should be zero for part time employee
+        */
         public void CalculateDiscountedPrice_PartTimeEmployee_NoDiscount()
         {
             var employee = new EmployeeDiscount
@@ -20,8 +22,10 @@ public class Tests
             Assert.AreEqual(1000, discountedPrice);
         }
         [Test]
-        //Test case for partial load employee. Testing whether choosing a PartialLoad employee type
-        // will provide a 5 percent discount
+        /*
+        Test case for partial load employee. Testing whether choosing a PartialLoad employee type
+        will provide a 5 percent discount
+        */
         public void CalculateDiscountedPrice_PartialLoadEmployee_FivePercentDiscount()
         {
             var employee = new EmployeeDiscount
@@ -35,8 +39,10 @@ public class Tests
             Assert.AreEqual(1880, discountedPrice);
         }
         [Test]
-        //Test case for Full-Time Employee . Testing whether choosing a Full Time employee type
-        // will provide a 10 percent discount
+        /*
+        Test case for Full-Time Employee . Testing whether choosing a Full Time employee type
+         will provide a 10 percent discount
+        */
         public void CalculateDiscountedPrice_FullTimeEmployee_TenPercentDiscount()
         {
             var employee = new EmployeeDiscount
@@ -50,8 +56,10 @@ public class Tests
             Assert.AreEqual(2670, discountedPrice);
         }
         [Test]
-        //Test case for CompanyPurchasing. Testing whether choosing a CompanyPurchasing type
-        // will provide a 20 percent discount
+        /*
+        Test case for CompanyPurchasing. Testing whether choosing a CompanyPurchasing type
+        will provide a 20 percent discount
+        */
         public void CalculateDiscountedPrice_CompanyPurchasing_TwentyPercentDiscount()
         {
             var employee = new EmployeeDiscount
@@ -65,8 +73,10 @@ public class Tests
             Assert.AreEqual(3950, discountedPrice);
         }
         [Test]
-        //Test case for Full-Time Employee with Zero Price. Testing whether choosing a Full Time employee type
-        // with Zero price will provide a valid return type
+        /*
+        Test case for Full-Time Employee with Zero Price. Testing whether choosing a Full Time employee type
+        with Zero price will provide a valid return type
+        */
         public void CalculateDiscountedPrice_FullTimeEmployee_InvalidPrice()
         {
             var employee = new EmployeeDiscount
@@ -80,8 +90,10 @@ public class Tests
             Assert.AreEqual(0, discountedPrice);
         }
         [Test]
-        //Test case for Full-Time Employee with Negative Price. Testing whether choosing a Full Time employee type
-        // with Negative price will provide a valid return type
+        /*
+        Test case for Full-Time Employee with Negative Price. Testing whether choosing a Full Time employee type
+        with Negative price will provide a valid return type
+        */
         public void CalculateDiscountedPrice_FullTimeEmployee_NegativePrice()
         {
             var employee = new EmployeeDiscount
